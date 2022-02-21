4997ab1f7c942e260ec1dd5bfbb9f504f384b589
Khalid Khushal <khaidkhushal.01@gmail.com>
2022-02-22 02:15:06 +0500

    First commit

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require('multer');
-const applicants = require("../model/schema");
+const applicants = require(".. / model / schema ");
 const fs = require('fs');
 const path = require('path');
 
